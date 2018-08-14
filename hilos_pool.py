a649287fba1c98e7b3a1c8a5722f70ea54276b73
formijor <formijor@gmail.com>
2018-08-14 18:49:03 -0300

    14/8 pruebas cliente/pool

--- hilos_pool.py
+++ hilos_pool.py
@@ -6,19 +6,31 @@ Created on 6 ago. 2018
 
 #https://medium.com/@betz.mark/cross-thread-event-dispatching-in-python-fc956446ad16
 
+#https://stackoverflow.com/questions/2933399/how-to-set-time-limit-on-raw-input
+#http://l4wisdom.com/python/python_threads.php
+#https://www.python-course.eu/input.php
+
 import threading
 import queue
 import time
 
 class hilo_pool():
     def __init__(self, conexion_archivo, cola_espera_in, cola_espera_out):
-        print (threading.currentThread().getName(), 'Iniciando')
+        print (threading.currentThread().getName(), 'Iniciando\n')
         self.conexion_hilo(conexion_archivo)     
         self.cola_espera_in = cola_espera_in
         self.cola_espera_out = cola_espera_out
-        time.sleep(2) 
-        self.enviar_mensaje_pool('\nAAAAAH\n---------------------\n')
-        self.recibir_mensajes_pool()
+        self.run()
+    
+    def run(self):
+        mensaje = self.recibir_mensajes_pool()
+        while mensaje != 'Desconectar':
+            mensaje = threading.currentThread().getName() + ': CONECTADO'#input(threading.currentThread().getName() + ': Esperando mensaje...\n')
+            self.enviar_mensaje_pool(mensaje)
+            mensaje = self.recibir_mensajes_pool()
+            self.imprimir_mensaje(mensaje)
+        print ('FIN: ' + threading.currentThread().getName())         
+        
         
     def conexion_hilo(self, conexion_archivo):
         pass    
@@ -30,10 +42,8 @@ class hilo_pool():
         pass
     
     def recibir_mensajes_pool(self):
-        while 1:
-            mensaje = self.cola_espera_in.get()
-            print (threading.currentThread().getName() + mensaje)  
-        
+        mensaje = self.cola_espera_in.get()
+        self.imprimir_mensaje('\nMensaje Recibido de pool para ' + threading.currentThread().getName() + ": " + mensaje)  
           
     def enviar_mensaje_pool(self, mensaje):
         self.cola_espera_out.put(mensaje)
@@ -49,40 +59,70 @@ class pool_hilo_principal():
         self.obtener_cantidad_threads()
         self.crear_cola()
         self.lista_hilos = {}
+        self.run_pool()    
+    
+    def run_pool(self):
+        cantidad_hilos_inicial = self.obtener_cantidad_threads()
         self.crear_hilos()
-        self.recibir_mensajes_hilos()               
-        self.finalizar_pool()        
+        time.sleep(2)
+        for hilo in self.lista_hilos.values():
+                hilo[1].put('Conectado\n')                
+        contador = 0     
+        while cantidad_hilos_inicial < self.obtener_cantidad_threads():
+            contador_cliente = 0
+            for cliente, hilo in self.lista_hilos.items():
+                contador_cliente += 1
+                print ('CANT_CLIENTES: ' + str(contador_cliente))
+                mensaje = hilo[2].get()
+                print('MENSSS:' + mensaje)
+                if mensaje == 'Desconectar':
+                    hilo[0].join()
+                elif mensaje == None:
+                    print('ENVIAR MENSAJE\n')
+                    cliente = input('CLIENTE: ')
+                    mensaje = input('MENSAJE: ')
+                    self.enviar_mensajes_hilos(cliente, mensaje)
+                else:
+                    self.imprimir_mensaje('Mensaje Recibido de ' + cliente + ' para Pool: ' + mensaje)
+            print(contador + 1)
+        print('FIN POOL')      
         
     def crear_hilos(self):        
-        cliente = ['Jorge', 'Carolina', 'doña']
+        cliente = ['Jorge', 'Carolina', 'Julio']
         for x in range(3):
-            nombre = cliente.pop()        
-            self.hilo = threading.Thread(target = hilo_pool, args = ("HOLA", self.cola_espera_in, self.cola_espera_out), name = nombre)
-            self.lista_hilos[nombre] = (self.hilo, self.cola_espera_in, self.cola_espera_out)
-            self.hilo.start()        
+            nombre = cliente.pop()    
+            cola_espera = self.crear_cola()   
+            self.hilo = threading.Thread(target = hilo_pool, args = ("HOLA", cola_espera[0], cola_espera[1]), name = nombre)
+            self.lista_hilos[nombre] = (self.hilo, cola_espera[0], cola_espera[1])
+            self.hilo.start()
             
     def recibir_mensajes_hilos(self):
         '''while threading.active_count() > self.thread_cantidad_inicial:        
         event = self.cola_espera_out.get()
         print (event)'''
-        time.sleep(6)
-        self.enviar_mensajes_hilos() 
+        pass
+        
+    #def recibir_mensaje_hilos(self):
+        
     
-    def enviar_mensajes_hilos(self):
-        print (self.lista_hilos)
-        hilo = self.lista_hilos['Carolina']
-        hilo[1].put('\nAy Caramba\n')
+    def enviar_mensajes_hilos(self, cliente, mensaje):
+        hilo = self.lista_hilos[cliente]
+        hilo[1].put(mensaje)
        
     def crear_cola(self):
-        self.cola_espera_in = queue.Queue()
-        self.cola_espera_out = queue.Queue()        
+        cola_espera_in = queue.Queue()
+        cola_espera_out = queue.Queue()
+        return (cola_espera_in, cola_espera_out)    
         
     def obtener_cantidad_threads(self):
-        self.thread_cantidad_inicial = threading.active_count()
-        print (self.thread_cantidad_inicial)
+        thread_cantidad_inicial = threading.active_count()
+        return thread_cantidad_inicial
     
     def hilo_esta_activo(self, hilo):
         return hilo.is_alive()        
+    
+    def imprimir_mensaje(self, mensaje):
+        print (mensaje)        
     
     def finalizar_pool(self):
         for hilo in self.lista_hilos.values():
